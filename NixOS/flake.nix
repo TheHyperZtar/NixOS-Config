cfc7cc0d9876bc7574d3a68d4b8686e9cb3c8d6e
TheHyperZtar <129815519+TheHyperZtar@users.noreply.github.com>
2024-06-30 15:26:27 -0700

    Update flake.nix

--- NixOS/flake.nix
+++ NixOS/flake.nix
@@ -17,6 +17,10 @@
 
   let
     system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+    lib = nixpkgs.lib;
   in
 
   {
@@ -25,7 +29,80 @@
         inherit system;
         modules = [
           disko.nixosModules.disko
-          (import ./disko.nix { device = "/dev/vda"; })
+          {
+            disko.devices = {
+              disk.main = {
+                device = "/dev/disk/by-id/some-disk-id";
+                type = "disk";
+                content = {
+                  type = "gpt";
+                  partitions = {
+                    THZ-BOOT = {
+                      name = "THZ-BOOT";
+                      size = "1G";
+                      type = "EF00";
+                      content = {
+                        type = "filesystem";
+                        format = "vfat";
+                        mountpoint = "/boot";
+                        extraArgs = [ "-n THZ-BOOT" ];
+                        mountOptions = [ "noatime" ];
+                      };
+                    };
+                    THZ-NixOS = {
+                      name = "THZ-NixOS";
+                      size = "100%";
+                      content = {
+                        type = "btrfs";
+                        extraArgs = [ "-f -L THZ-NixOS" ];
+                        subvolumes = {
+                          "/root" = {
+                            mountOptions = [ "noatime" ];
+                            mountpoint = "/";
+                          };
+
+                          "/home" = {
+                            mountOptions = [ "noatime" ];
+                            mountpoint = "/home";
+                          };
+
+                          "/nix" = {
+                            mountOptions = [ "noatime" ];
+                            mountpoint = "/nix";
+                          };
+                        };
+                      };
+                    };
+                  };
+                };
+              };
+            };  
+            fileSystems = lib.mkForce {
+              "/" = {
+                device = "/dev/disk/by-label/THZ-NixOS";
+                fsType = "btrfs";
+                options = [ "subvol=/root" "noatime" ];
+                };
+
+              "/boot" = {
+                device = "/dev/disk/by-label/THZ-BOOT";
+                fsType = "vfat";
+                options = [ "noatime" ];
+              };
+
+              "/home" = {
+                device = "/dev/disk/by-label/THZ-NixOS";
+                fsType = "btrfs";
+                options = [ "subvol=/home" "noatime" ];
+              };
+
+              "/nix" = {
+                device = "/dev/disk/by-label/THZ-NixOS";
+                fsType = "btrfs";
+                options = [ "subvol=/nix" "noatime" ];
+              };
+            };
+          }
           ./hosts/THZ-VM/configuration.nix
         ];
       };
